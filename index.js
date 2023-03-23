99bc87f0e97a16cb24467174ecc5628aeeabbdc3
Job koech <jobkoech610@gmail.com>
2023-03-23 19:38:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  console.log("hey boy");
+
+  let target = document.getElementById("text");
+  target.textContent= "This is really cool!";
